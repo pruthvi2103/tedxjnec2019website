904fb9c32f295e272b5e29624d973fbd84dce52d
ZeffK <54438151+ZeffK@users.noreply.github.com>
2019-08-28 00:21:08 +0530

    INDEX MASONRY
    the grid lay out was not responsive , if the mobile was placed horizontally the masonry container was going out of page , i have fixed that

--- tedxjnec website soham SAPERA/Speakers/js/support.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled',$(this).scrollTop()>50);
-});
-$(function(){
-    $('#mainfooter').load('footer.html');
-    $('#navigation').load('navbar.html');
-});
